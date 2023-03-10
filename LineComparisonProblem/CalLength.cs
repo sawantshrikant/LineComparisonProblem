4e1892d2a9c215db0515c3ef6caf2665a56eb06e
Shrikant <sawantshrikant07@gmail.com>
2023-03-10 16:34:50 +0530

    [Shrikant] add:EqualityOfTwoLines

--- LineComparisonProblem/CalLength.cs
+++ LineComparisonProblem/CalLength.cs
@@ -1,22 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class CalLength
+namespace LineComparision
 {
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
-
-    public CalLength(int x1, int y1, int x2, int y2)
+    public class CalculateLength
     {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
-
-    public double GetLength()
-    {
-        return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        int x1, y1, x2, y2;
+        public CalculateLength(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public double calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return length;
+        }
+        public void equalChceck(double line1, double line2)
+        {
+            if (line1.Equals(line2))
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
+        }
     }
 }
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
-
-public class Program
+namespace LineComparision
 {
-    public static void Main()
+    public class Program
     {
-        Console.WriteLine("Welcome to Line Comparison Computation Program");
-
-        Console.Write("Enter x1: ");
-        int x1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter y1: ");
-        int y1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter x2: ");
-        int x2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter y2: ");
-        int y2 = Convert.ToInt32(Console.ReadLine());
-
-        CalLength line = new CalLength(x1, y1, x2, y2);
-        double length = line.GetLength();
-
-        Console.WriteLine("Length of the line: " + length);
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to line calculator");
+            Console.WriteLine("Enter x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            CalculateLength calculateLength = new CalculateLength(x1, y1, x2, y2);
+            double line1 = calculateLength.calculate();
+            Console.WriteLine("Enter x3: ");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y3: ");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x4: ");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y4: ");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+            CalculateLength calculateLength1 = new CalculateLength(x3, y3, x4, y4);
+            double line2 = calculateLength.calculate();
+            calculateLength1.equalChceck(line1, line2);
+        }
     }
 }
