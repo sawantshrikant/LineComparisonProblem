079a8c098730b847fee2caf59791abd1fdd4169f
Shrikant <sawantshrikant07@gmail.com>
2023-03-10 16:09:52 +0530

    [Shrikant] add:CalculateLengthOfLine

---
+++ LineComparisonProblem/CalLength.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class CalLength
+{
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public CalLength(int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double GetLength()
+    {
+        return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 
-namespace LineComparisonProblem
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        Console.WriteLine("Welcome to Line Comparison Computation Program");
 
-        }
+        Console.Write("Enter x1: ");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter y1: ");
+        int y1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter x2: ");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter y2: ");
+        int y2 = Convert.ToInt32(Console.ReadLine());
+
+        CalLength line = new CalLength(x1, y1, x2, y2);
+        double length = line.GetLength();
+
+        Console.WriteLine("Length of the line: " + length);
     }
 }
